667638336edbdf3b15c60423088cbf37357af3bd
Pramesh Karki <Parameswarkarki@gmail.com>
2021-06-27 10:22:34 +0545

    Initial Commit

---
+++ src/SnakeGame.java
@@ -0,0 +1,9 @@
+public class SnakeGame {
+    public static void main(String[] args) {
+       new GameFrame();
+    }
+}
+//Pramesh Karki
+//karkipramesh.com.np
+//All credit of this code goes to @BroCode,If you want to learn such great stuff please check this youtube channel out!
+
